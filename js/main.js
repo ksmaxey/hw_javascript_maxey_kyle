b45d9485e2a80575923aeae91d28a5d156cdcdb4
Kyle Maxey <ksmaxey@gmail.com>
2017-01-25 17:44:22 -0500

    create presidents branch
    Created presidents branch and updated javascript file with presidential
    array.  Commented out individual objects to show the processes of the
    assignment.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
 var icecream = ['Cookie Dough', 'Neapolitan', 'Mint Chocolate Chip'];
 icecream.push('Caramel Crunch');
+
+/* var trump = {
+    firstName: 'Donald',
+    lastName: 'Trump',
+    termLength: '4 Years',
+    yearsOfPresidency: '2017'
+};
+
+var obama = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: '8 Years',
+    yearsOfPresidency: '2009-2017'
+};
+
+var wbush = {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: '8 Years',
+    yearsOfPresidency: '2001-2009'
+};
+
+var clinton = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '8 Years',
+    yearsOfPresidency: '1993-2001'
+};
+
+var hbush = {
+    firstName: 'George H. W.',
+    lastName: 'Bush',
+    termLength: '4 Years',
+    yearsOfPresidency: '1989-1993'
+}; */
+
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: '4 Years', yearsOfPresidency: '2017'},
+    {firstName: 'Barack', lastName: 'Obama', termLength: '8 Years', yearsOfPresidency: '2009-2017'},
+    {firstName: 'George W.', lastName: 'Bush', termLength: '8 Years', yearsOfPresidency: '2001-2009'},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: '8 Years', yearsOfPresidency: '1993-2001'},
+    {firstName: 'George H. W.', lastName: 'Bush', termLength: '4 Years', yearsOfPresidency: '1989-1993'},
+];
+
+console.log(presidents[2].firstName, presidents[2].lastName);
